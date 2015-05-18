671b16ad582268328511800d40b32ea2da5443b9
Samuel Chiang <samuelc@workssys.com>
2015-05-18 10:30:24 -0700

    Add description on function declaration

--- xep-0047/include/wksxep0047.h
+++ xep-0047/include/wksxep0047.h
@@ -6,15 +6,15 @@
 #define MAX_HEAD_LEN	128
 #define MAX_LEN 50
 
-
+/* Create a libstrophe context, create a connection, initiate connection */
 xmpp_conn_t*  XMPP_Init(char* jid, char* pass, char* host, xmpp_ctx_t  **pctx);
 
 void XMPP_Presence(xmpp_conn_t* conn);
 
 //void XMPP_IBB_SendPayload(xmpp_conn_t * const conn, xmpp_stanza_t * const stanza, void * const userdata, char *);
-
+/* Send a presence to XMPP Server*/
 void XMPP_Echo_Test(xmpp_conn_t * const conn, xmpp_stanza_t * const stanza, void * const userdata);
-
+/* Send XEP-0047 result stanza */
 void XMPP_IBB_Ack_Send(xmpp_conn_t * const conn, xmpp_stanza_t * const stanza, void * const userdata);
 
 void XMPP_XEP0047_Init(xmpp_conn_t* conn, xmpp_ctx_t* ctx);
