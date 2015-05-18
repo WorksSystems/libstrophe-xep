58b68abbe6847f9a1686b0a32b90bcf6df55d311
root <root@jeremy-vm7.workssys>
2015-05-18 21:06:19 +0800

    Fix test program

--- xep-0047/example/main.c
+++ xep-0047/example/main.c
@@ -14,21 +14,50 @@
 xmpp_conn_t*  XMPP_Init(char* jid, char* pass, char* host, xmpp_ctx_t  **pctx);
 void XMPP_Close(xmpp_ctx_t *ctx,  xmpp_conn_t *conn);
 
+xmpp_conn_t* conn = NULL;
+xmpp_ctx_t *ctx = NULL;
+
 pthread_attr_t attr1;
 pthread_t pid1;
+
+
+void Get_IBB_Data()
+{
+#if 1
+    while(1)
+    {
+        sleep(1);
+
+        if(XMPP_IBB_Get_Recv() != NULL)
+        {
+	    printf("Get IBB Data=[%s]\n", XMPP_IBB_Get_Recv());
+	    if(XMPP_IBB_Get_gStanza() !=NULL)
+            	 XMPP_IBB_SendPayload(conn, XMPP_IBB_Get_gStanza(), \
+		  (void*)ctx, XMPP_IBB_Get_Recv() );//Echo test
+
+   //         XMPP_IBB_Reset_Recv();
+ //           XMPP_IBB_Reset_gStanza();
+        }
+
+    }
+#endif
+
+}
 
 int  main(int argc, char **argv)
 {
  
-    xmpp_conn_t* conn = NULL;
-    xmpp_ctx_t *ctx = NULL;    
+ //   xmpp_conn_t* conn = NULL;
+ //   xmpp_ctx_t *ctx = NULL;    
     char *jid, *pass, *host;	
     int errornum=0;
     void *ptr;
     pthread_attr_init(&attr1);
     pthread_attr_setstacksize(&attr1, PTHREAD_STACK_MIN * 2);
 
-      
+   // pthread_create(&pid1, \
+        &attr1, (void*)Get_IBB_Data, NULL );    
+  
     /* take a jid and password on the command line */
     if (argc < 3 || argc > 4) {
         fprintf(stderr, "Usage: xep0047-test  <jid> <pass> [<host>]\n\n");
@@ -44,27 +73,13 @@ int  main(int argc, char **argv)
 
     conn = XMPP_Init(jid , pass, host, &ctx);
 
+
+     pthread_create(&pid1, \
+        &attr1, (void*)Get_IBB_Data, NULL );
+
     if(ctx !=NULL)
-        pthread_create(&pid1, \
-        &attr1, (void*)xmpp_run, (void*)ctx );
-
-    
-    while(1)
-    {
-        sleep(1);
-        
-        if(XMPP_IBB_Get_Recv() != NULL)
-	{
-            XMPP_IBB_SendPayload(conn, XMPP_IBB_Get_gStanza(), \
-   (void*)ctx, XMPP_IBB_Get_Recv() );//Echo test            
-            
-            XMPP_IBB_Reset_Recv(); 
-            XMPP_IBB_Reset_gStanza(); 
-
-        }
-
-    }
-  
+        xmpp_run(ctx);
+ 
     xmpp_stop(ctx);
     sleep(1);
     XMPP_Close(ctx, conn);
